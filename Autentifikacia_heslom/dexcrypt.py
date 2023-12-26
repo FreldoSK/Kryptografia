db1084b590d2244d386c673fed921edbf44bf127
Peter Kolok <peterkolok@gmail.com>
2023-12-26 12:34:26 +0100

    test

--- Autentifikacia_heslom/dexcrypt.py
+++ Autentifikacia_heslom/dexcrypt.py
@@ -2,6 +2,7 @@ import itertools
 import string
 from hashlib import md5
 from base64 import b64encode
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def crypt(passwd, salt):
     m = md5()
@@ -9,28 +10,34 @@ def crypt(passwd, salt):
     m.update(salt.encode('utf-8'))
     return b64encode(m.digest()).decode('utf-8')
 
+def attempt_passwords(username, salt, hashed):
+    for length in range(4, 5):  
+        for passwd in itertools.product(string.ascii_letters + string.digits, repeat=length):
+            generated_passwd = ''.join(passwd)
+            print(f"Trying password: {generated_passwd} for user: {username}")  
+            if crypt(generated_passwd, salt) == hashed:
+                return username, generated_passwd
+    return username, None
 
 shadow_entries = {}
-with open(r"..\Autentifikacia_heslom\shadow1.txt", "r") as file:
+with open(r".\Autentifikacia_heslom\shadow1.txt", "r") as file:
     for line in file:
         parts = line.strip().split(':')
         if len(parts) == 3:
             username, salt, hashed = parts
             shadow_entries[username] = (salt, hashed)
 
-
 found_passwords = {}
 
-for username, (salt, hashed) in shadow_entries.items():
-    for length in range(4, 6):  
-        for passwd in itertools.product(string.ascii_letters + string.digits, repeat=length):
-            generated_passwd = ''.join(passwd)
-            print(f"Trying password: {generated_passwd} for user: {username}")  
-            if crypt(generated_passwd, salt) == hashed:
-                found_passwords[username] = generated_passwd
-                break  
-    if username not in found_passwords:
-        print(f"No password found for {username}")
+with ThreadPoolExecutor(max_workers=10) as executor:
+    futures = {executor.submit(attempt_passwords, user, data[0], data[1]): user for user, data in shadow_entries.items()}
+    for future in as_completed(futures):
+        user, found_passwd = future.result()
+        if found_passwd:
+            found_passwords[user] = found_passwd
+            print(f"Password found: {found_passwd} for user: {user}")
+        else:
+            print(f"No password found for {user}")
 
 
 for user, passwd in found_passwords.items():
